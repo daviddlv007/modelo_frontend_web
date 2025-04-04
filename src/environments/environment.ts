92d139491861c0e7efd1d0b874e592cae1a9d90c
daviddlv007 <davidlanzadlv7@gmail.com>
2025-04-04 15:05:11 -0400

    Actualización de la aplicación con nuevos cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'
+    //apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://ec2-3-94-10-120.compute-1.amazonaws.com:8080'
   };
-  
