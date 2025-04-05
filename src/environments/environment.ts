67796ebde454ebfed74ece843ec9387097d66957
daviddlv007 <davidlanzadlv7@gmail.com>
2025-04-04 21:07:10 -0400

    Actualización de la aplicación con nuevos cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     //apiUrl: 'https://localhost:8443'
     //apiUrl: 'http://localhost:8080'
     //apiUrl: 'http://ec2-3-94-10-120.compute-1.amazonaws.com:8080'
-    apiUrl: 'https://ec2-3-94-10-120.compute-1.amazonaws.com:8443'
+    apiUrl: 'https://proyectosi2.duckdns.org:8443'
   };
