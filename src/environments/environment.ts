26a650ba806ad93d1b914d9b5c13ffa67a338b9a
daviddlv007 <davidlanzadlv7@gmail.com>
2025-04-06 00:22:42 -0400

    Tu mensaje de commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
+//frontend\src\environments\environment.ts
+
 export const environment = {
-    production: false,
-    //apiUrl: 'https://localhost:8443'
-    //apiUrl: 'http://localhost:8080'
-    //apiUrl: 'http://ec2-3-94-10-120.compute-1.amazonaws.com:8080'
-    apiUrl: 'https://proyectosi2.duckdns.org:8443'
-  };
+  production: false,
+  apiUrl: 'http://localhost:8080'
+};
